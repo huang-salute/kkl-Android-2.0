8c039f50e3b20b786bb5fd4cd966fe0da8318b14
huang-salute <132679876+huang-salute@users.noreply.github.com>
2023-10-16 19:36:24 +0800

    Update bot.py

--- kkl_bot/bot.py
+++ kkl_bot/bot.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_plugins(path.join(path.dirname(__file__), 'kkl', 'plugins'),
         'kkl.plugins')
-    nonebot.run(host='127.0.0.2', port=8080)
+    nonebot.run(host='127.0.0.1', port=8080)
